f65539ed4073268486cb73448e24ca3978b845d1
Takodakko <monchichi@Monchichis-Air.lan>
2024-02-09 17:08:30 -0500

    updating where card state is kept

--- src/hanafuda/Card.tsx
+++ src/hanafuda/Card.tsx
@@ -1,13 +1,9 @@
 
+function Card(props: {blank: boolean, flipCard: Function, id: string, chosenCard: string}) {
 
-function Card(props: {blank: boolean}) {
-
-    const cardList = ['january_crane', 'january_scroll', 'january_plain1', 'january_plain2', 'february_bird', 'february_scroll', 'february_plain1', 'february_plain2', 'march_curtain', 'march_scroll', 'march_plain1', 'march_plain2', 'april_bird', 'april_scroll', 'april_plain1', 'april_plain2', 'may_dock', 'may_scroll', 'may_plain1', 'may_plain2', 'june_butterfly', 'june_scroll', 'june_plain1', 'june_plain2', 'july_boar', 'july_scroll', 'july_plain1', 'july_plain2', 'august_moon', 'august_geese', 'august_plain1', 'august_plain2', 'september_sake', 'september_scroll', 'september_plain1', 'september_plain2', 'october_deer', 'october_scroll', 'october_plain1', 'october_plain2', 'november_man', 'november_bird', 'november_bird', 'november_thunder', 'december_phoenix', 'december_plain1', 'december_plain2', 'december_plain3'];
-    const randomNumber = Math.floor(Math.random() * cardList.length);
-    const chosenCard = cardList[randomNumber];
     return (
         <>
-          {!props.blank ? <img alt={chosenCard} src={`../src/assets/cards/${chosenCard}.jpg`} height="100px" width="65px"></img> : <img height="100px" width="65px" alt="back of card" src="../src/assets/cards/card_back.jpg"></img>}
+          {!props.blank ? <img id={props.id} alt={props.chosenCard} src={`../src/assets/cards/${props.chosenCard}.jpg`} height="100px" width="65px"></img> : <img id={props.id} onClick={props.flipCard} height="100px" width="65px" alt="back of card" src="../src/assets/cards/card_back.jpg"></img>}
         </>
     )
 }
--- src/hanafuda/Hanafuda.tsx
+++ src/hanafuda/Hanafuda.tsx
@@ -3,6 +3,34 @@ import Card from './Card';
 
 function Hanafuda() {
   const [youWin, setYouWin] = useState(false);
+  const [faceDownCards, setFaceDownCards] = useState([true, true, true]);
+  const cardList = ['january_crane', 'january_scroll', 'january_plain1', 'january_plain2', 'february_bird', 'february_scroll', 'february_plain1', 'february_plain2', 'march_curtain', 'march_scroll', 'march_plain1', 'march_plain2', 'april_bird', 'april_scroll', 'april_plain1', 'april_plain2', 'may_dock', 'may_scroll', 'may_plain1', 'may_plain2', 'june_butterfly', 'june_scroll', 'june_plain1', 'june_plain2', 'july_boar', 'july_scroll', 'july_plain1', 'july_plain2', 'august_moon', 'august_geese', 'august_plain1', 'august_plain2', 'september_sake', 'september_scroll', 'september_plain1', 'september_plain2', 'october_deer', 'october_scroll', 'october_plain1', 'october_plain2', 'november_man', 'november_bird', 'november_bird', 'november_thunder', 'december_phoenix', 'december_plain1', 'december_plain2', 'december_plain3'];
+  const [cardValues, setCardValues] = useState(initializeCardValues());
+
+  function initializeCardValues() {
+    const cards = [];
+    for (let i = 0; i < 6; i++) {
+      const randomNumber = Math.floor(Math.random() * cardList.length);
+      cards.push(cardList[randomNumber]);
+    }
+    return cards;
+  }
+
+  function resetCardValues() {
+    const newValues = initializeCardValues();
+    setCardValues(newValues);
+    setFaceDownCards([true, true, true]);
+  }
+
+  function flipCard(e: React.MouseEvent<HTMLImageElement, MouseEvent>) {
+    console.log(e);
+    if (e.target) {
+      const cardToChange = parseInt(e.target.id, 10);
+      const newState = [...faceDownCards];
+      newState[cardToChange] = false;
+      setFaceDownCards(newState);
+    }
+  }
 
   return (
     <>
@@ -12,27 +40,28 @@ function Hanafuda() {
         <br></br>
         <div className="cardrow">
           <div className="cardbox">
-            <Card blank={false}/>
+            <Card blank={false} flipCard={flipCard} id="3" chosenCard={cardValues[0]}/>
           </div>
           <div className="cardbox">
-            <Card blank={false}/>
+            <Card blank={false} flipCard={flipCard} id="4" chosenCard={cardValues[1]}/>
           </div>
           <div className="cardbox">
-            <Card blank={false}/>
+            <Card blank={false} flipCard={flipCard} id="5" chosenCard={cardValues[2]}/>
           </div>
         </div>
         <br></br>
         <div className="cardrow">
           <div className="cardbox">
-            <Card blank={true}/>
+            <Card blank={faceDownCards[0]} flipCard={flipCard} id="0" chosenCard={cardValues[3]}/>
           </div>
           <div className="cardbox">
-            <Card blank={true}/>
+            <Card blank={faceDownCards[1]} flipCard={flipCard} id="1" chosenCard={cardValues[4]}/>
           </div>
           <div className="cardbox">
-            <Card blank={true}/>
+            <Card blank={faceDownCards[2]} flipCard={flipCard} id="2" chosenCard={cardValues[5]}/>
           </div>
         </div>
+        <button onClick={resetCardValues}>Reset</button>
       </div>}
     </>
   )
