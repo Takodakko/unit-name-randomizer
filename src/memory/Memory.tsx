a29976e0677c79989e3020f26a8685991cf1ed5d
Takodakko <monchichi@Monchichis-Air.lan>
2024-03-01 17:38:32 -0500

    fixed up flipping and scoring in memory game

--- src/memory/Memory.tsx
+++ src/memory/Memory.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Card from '../hanafuda/Card';
 
 interface aCard {
@@ -7,6 +7,7 @@ interface aCard {
     monthValue: string;
     readableName: string;
     faceUp: boolean;
+    scored: boolean;
   }
 
   interface GameState {
@@ -21,6 +22,21 @@ interface aCard {
     [key: string]: number;
   }
 
+  const initialMonthMatching = {
+    january: 0,
+    february: 0,
+    march: 0,
+    april: 0,
+    may: 0,
+    june: 0,
+    july: 0,
+    august: 0,
+    september: 0,
+    october: 0,
+    november: 0,
+    december: 0
+  }
+
 function Memory() {
     const cardList = ['january_crane', 'january_scroll', 'january_plain1', 'january_plain2', 'february_bird', 'february_scroll', 'february_plain1', 'february_plain2', 'march_curtain', 'march_scroll', 'march_plain1', 'march_plain2', 'april_bird', 'april_scroll', 'april_plain1', 'april_plain2', 'may_dock', 'may_scroll', 'may_plain1', 'may_plain2', 'june_butterfly', 'june_scroll', 'june_plain1', 'june_plain2', 'july_boar', 'july_scroll', 'july_plain1', 'july_plain2', 'august_moon', 'august_geese', 'august_plain1', 'august_plain2', 'september_sake', 'september_scroll', 'september_plain1', 'september_plain2', 'october_deer', 'october_scroll', 'october_plain1', 'october_plain2', 'november_man', 'november_bird', 'november_bird', 'november_thunder', 'december_phoenix', 'december_plain1', 'december_plain2', 'december_plain3'];
     // const rowLength = 6;
@@ -30,8 +46,14 @@ function Memory() {
         faceDownCards: [],
         faceUpCards: [],
         matchingCards: [],
-        currentMonth: {}
+        currentMonth: initialMonthMatching
     };
+    
+    const refStorage = new Map();
+    for (let i = 0; i < 48; i++) {
+      const newRef = useRef(null);
+      refStorage.set(i.toString(), newRef);
+    }
     const initialCardSet: aCard[] = [];
     const [gameState, setGameState] = useState(initialState);
     const [cardSet, setCardSet] = useState(initialCardSet);
@@ -50,6 +72,7 @@ function Memory() {
             id: i.toString(),
             monthValue: copyCardList[randomNumber].slice(0, copyCardList[randomNumber].indexOf('_')),
             faceUp: true,
+            scored: false
           };
           cards.push(newCard);
           copyCardList.splice(randomNumber, 1);
@@ -58,6 +81,7 @@ function Memory() {
         const copyGameState = {...gameState};
         copyGameState.faceUpCards = [];
         copyGameState.faceDownCards = [...cards];
+        copyGameState.matchingCards = [];
         setGameState(copyGameState);
       }
     
@@ -67,6 +91,9 @@ function Memory() {
       const copyGameState = {...gameState};
       const arrayToUse = target.alt !== 'back of card' ? copyGameState.faceUpCards : copyGameState.faceDownCards;
       const indexToChange = arrayToUse.findIndex((card) => card.id === id);
+      if (indexToChange === -1) {
+        return;
+      }
       const cardToChange = arrayToUse[indexToChange];
 
       if (target.alt === 'back of card') {
@@ -74,12 +101,24 @@ function Memory() {
             return;
         }
         cardToChange.faceUp = true;
-        if (copyGameState.currentMonth[cardToChange.monthValue] < 2) {
+        const currentCardElement = refStorage.get(cardToChange.id);
+        if (copyGameState.currentMonth[cardToChange.monthValue] === 1) {
+            currentCardElement.current.className = 'cardbox cardboxscoring';
+            const ids:string[] = [];
             copyGameState.matchingCards.push(...copyGameState.faceUpCards);
+            copyGameState.matchingCards.push(cardToChange);
+            copyGameState.faceUpCards.forEach((card) => {
+              card.scored = true;
+              ids.push(card.id);
+            });
+            ids.forEach((id) => {
+              const otherScoringCard = refStorage.get(id);
+              otherScoringCard.current.className = 'cardbox cardboxscoring';
+            })
+            
             copyGameState.faceUpCards.pop();
             copyGameState.faceUpCards.pop();
             copyGameState.currentMonth[cardToChange.monthValue] = 0;
-            // target.parentElement.className = 'cardboxscoring';
         } else {
             copyGameState.currentMonth[cardToChange.monthValue] += 1;
             copyGameState.faceUpCards.push(cardToChange);
@@ -99,7 +138,8 @@ function Memory() {
         const deck: JSX.Element[] = [];
             for (let i = 0; i < cardSet.length; i++) {
                 const currentCard = cardSet[i];
-                const aNewCard = <div className="cardbox" key={currentCard.id}>
+                const divRef = refStorage.get(currentCard.id);
+                const aNewCard = <div className="cardbox" key={currentCard.id} ref={divRef}>
                 <Card monthValue={currentCard.monthValue} faceUp={currentCard.faceUp} flipCard={(e: React.MouseEvent<HTMLElement, MouseEvent>) => flipCard(currentCard.id, e)} id={currentCard.id} chosenCard={currentCard.value} altText={currentCard.readableName}/>
               </div>
               deck.push(aNewCard);
@@ -116,8 +156,10 @@ function Memory() {
     function resetGame() {
       if (!played) setPlayed(true);
       initializeCardValues();
-    //   setGameState(initialState);
       setIsPlaying(false);
+      refStorage.forEach((val) => {
+        if (val.current) val.current.className = 'cardbox';
+      })
     }
 
     function startGame() {
